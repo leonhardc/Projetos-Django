748286cf1248db4cf3983c9c686f20669997a48e
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-03-28 21:41:39 -0300

    Adicionado interface administrativa no App da Agenda

--- ProjetoAgenda/contatos/admin.py
+++ ProjetoAgenda/contatos/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    # exibir na interface admin os campos abaixo
+    list_display = ['id', 'nome', 'sobrenome', 'telefone', 'email',
+                    'data_criacao', 'categoria']
+
+    # tornar os campos abaixo clicáveis
+    list_display_links = ['id', 'nome', 'sobrenome']
+
+    # acrescentar filtros por nome e sobrenome
+    list_filter = ['nome', 'sobrenome']
+
+    # exibir somente 10 elementos por página
+    list_per_page = 10
+    
+    # adiciona fields de busca
+    search_fields = ['nome', 'sobrenome']
+
+
+# registrar as classes abaixo na interface de admin
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
