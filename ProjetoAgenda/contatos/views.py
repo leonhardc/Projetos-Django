078dba59e932a840e5516fde6341505d0b8fd108
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-03-29 18:44:53 -0300

    Correção de tratamento de erro. Erro 404 - Page not Found

--- ProjetoAgenda/contatos/views.py
+++ ProjetoAgenda/contatos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Contato
 
 
@@ -10,7 +11,25 @@ def index(request):
 
 
 def ver_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
-    return render(request, 'contatos/ver_contato.html', {
-        'contato': contato
-    })
+    try:
+        contato = Contato.objects.get(id=contato_id)
+        return render(request, 'contatos/ver_contato.html', {
+            'contato': contato
+        })
+    except Contato.DoesNotExist as e:
+        print(e)
+        # O erro abaixo se refere a uma página web não encontrada.
+        return Http404()
+
+# outra forma de fazer a view 'ver_contato'
+#
+#
+#
+# from django.shortcuts import get_object_or_404
+# def ver_contato(request, contato_id):
+#     contato = Contato.objects.get(id=contato_id)
+#     contato = get_object_or_404(Contato, id=contato_id)
+#     return render(request, 'contatos/ver_contato.html', {
+#         'contato': contato
+#     })
+
