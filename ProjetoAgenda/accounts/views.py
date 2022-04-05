4bbf4f63886ed330b96a67222f251a29b9303dda
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-04-05 17:52:26 -0300

    Adicionado view 'register', para cadastrar um usuário

--- ProjetoAgenda/accounts/views.py
+++ ProjetoAgenda/accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.validators import validate_email # validar email
+from django.contrib.auth.models import User # checar a existência de um usuário na base de dados
 
 
 def login(request):
@@ -11,8 +13,64 @@ def logout(request):
 
 
 def register(request):
-    print('>>> ', request.POST)
-    return render(request, 'accounts/register.html')
+    # checando o formulário está vazio.
+    if request.method != 'POST':
+        messages.info(request, 'NADA POSTADO.')
+        return render(request, 'accounts/register.html')
+    # declarando variáveis
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    email = request.POST.get('email')
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    senha_dois = request.POST.get('senha2')
+    # fazendo checagem das variáveis
+    if not nome or not sobrenome or not email or not usuario or not senha or not senha_dois:
+        messages.error(request, 'Nenhum dos campos pode ser vazio.')
+
+    try:
+        # validação de email
+        validate_email(email)
+
+    except:
+        messages.error(request, 'O campo "Email" não é válido.')
+        return render(request, 'accounts/register.html')
+
+    if len(senha) < 6:
+        # validação do tamanho da senha
+        messages.error(request, 'Senha precisa ter 6 caracteres ou mais.')
+        return render(request, 'accounts/register.html')
+
+    if len(usuario) < 6:
+        # validação do tamanho do usuário
+        messages.error(request, 'Usuário precisa ter 6 caracteres ou mais.')
+        return render(request, 'accounts/register.html')
+
+    if senha != senha_dois:
+        # validação se a primeira senha coincide com a segunda senha.
+        messages.error(request, 'Senhas não são iguais.')
+        return render(request, 'accounts/register.html')
+
+    if User.objects.filter(username=usuario).exists():
+        # checa se o usuário digitado no formulário existe na base de dados
+        messages.error(request, 'Usuário já existe.')
+        return render(request, 'accounts/register.html')
+
+    if User.objects.filter(email=email).exists():
+        # checa se o usuário digitado no formulário existe na base de dados
+        messages.error(request, 'Email já existe.')
+        return render(request, 'accounts/register.html')
+
+    messages.success(request, 'Registrado com Sucesso.')
+    # registrar o usuário na base de dados
+    user = User.objects.create_user(username=usuario,
+                                    email=email,
+                                    password=senha,
+                                    first_name=nome,
+                                    last_name=sobrenome)
+    user.save()
+    # redireciona para tela de login
+    return redirect('login')
 
 
 def dashboard(request):
