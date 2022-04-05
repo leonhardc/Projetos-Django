10cbbfa89442a75d91deb4272ccbcfb4ee88540f
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-04-05 16:26:32 -0300

    Excluido projeto de Gerenciamento de Funcionários (projeto corrompido) - Irei Recomeçar outro dia

--- GerenciamentoDeFuncionarios/ger_func/website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'website'
-
-# urlpatterns contem a lista de reteamento de URLs
-urlpatterns = [
-    path('', views.index, name='index')
-]
