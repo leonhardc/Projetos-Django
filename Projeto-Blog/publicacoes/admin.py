a8731201379b34c8f138a7d79084de307b4c69d6
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-07-17 14:56:45 -0300

    Adicionadas referencias do antigo app posts

--- Projeto-Blog/publicacoes/admin.py
+++ Projeto-Blog/publicacoes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Publicacoes
 
 # Register your models here.
+class PublicacoesAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'titulo_post',
+        'autor_post',
+        'data_post',
+        'categoria_post',
+        'publicado_post',
+    )
+    list_editable = (
+        'publicado_post',
+    )
+    list_display_links = (
+        'id',
+        'titulo_post',
+    )
+
+admi.site.register(Publicacoes, PublicacoesAdmin)
