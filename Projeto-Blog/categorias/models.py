8bc48d5c56b4b42bfc04c2ee1c24b46cc0099f8c
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-07-17 19:52:31 -0300

    Adicionado verbose_name= nos campos dos models

--- Projeto-Blog/categorias/models.py
+++ Projeto-Blog/categorias/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
 class Categoria(models.Model):
-    nome_cat = models.CharField(max_length=50) # nome da categoria
+    nome_cat = models.CharField(max_length=50, verbose_name='Categoria') # nome da categoria
+
+    def __str__(self):
+        return self.nome_cat
