205af14ecb24c115e7fee0f7e1ddb386e9231d01
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-07-18 16:55:06 -0300

    Implementado ViewClass PostIndex()

--- Projeto-Blog/publicacoes/views.py
+++ Projeto-Blog/publicacoes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from publicacoes.models import Publicacoes # para PostIndex()
 
 # Create your views here.
 class PostIndex(ListView):
-    pass
+    model = Publicacoes
+    template_name = 'publicacoes/index.html'
+    paginate_by = 3
+    context_object_name = 'publicacoes' # obejto iteravel que setá usado em index.html
 
 class PostBusca(PostIndex):
     pass
