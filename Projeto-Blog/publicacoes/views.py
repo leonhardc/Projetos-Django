19acc98bb910246cb10d1df06d35707a13bd3efb
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-07-19 19:56:23 -0300

    Adicionadas alterações para injetar numero de comentarios publicados na página

--- Projeto-Blog/publicacoes/views.py
+++ Projeto-Blog/publicacoes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from publicacoes.models import Publicacoes # para PostIndex()
+from django.db.models import Q, Count, Case, When # para ejetar numero de comentarios em
+                                                  # PostIndex >> get_queryset()
 
 # Create your views here.
 class PostIndex(ListView):
@@ -13,7 +15,18 @@ class PostIndex(ListView):
     def get_queryset(self): # sobrescrever método para mudar a ordem de exibição das
                             # publicoes na página
         qs = super().get_queryset()
-        qs = qs.order_by('-id')
+        qs = qs.order_by('-id').filter(publicado_post=True)
+        # ejetar na consulta o numero de comentarios publicados
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(
+                        comentario__publicado_comentario=True,
+                        then=1
+                    )
+                )
+            )
+        )
         return qs
 
 class PostBusca(PostIndex):
