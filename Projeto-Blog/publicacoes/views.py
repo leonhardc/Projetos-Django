68add1464595b08ff3a6c1c89dab26294a40263d
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-07-20 20:58:51 -0300

    Implementado view PostCategotia

--- Projeto-Blog/publicacoes/views.py
+++ Projeto-Blog/publicacoes/views.py
@@ -30,10 +30,22 @@ class PostIndex(ListView):
         return qs
 
 class PostBusca(PostIndex):
-    pass
+    template_name = 'publicacoes/post_busca.html'
 
 class PostCategoria(PostIndex):
     template_name = 'publicacoes/post_categoria.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        categoria = self.kwargs.get('categoria', None)
+        if not categoria:
+            return qs
+
+        qs = qs.filter(
+            categoria_post__nome_cat__iexact=categoria
+        )
+
+        return qs
 
 
 class PostDetalhes(UpdateView):
